d86d75ca5978810f0b1847caea4006ef3081e5f7
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-12-05 21:30:09 -0400

    oops

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -13,10 +13,10 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-export PRE_BUILD_CMDS='
+export PRE_BUILD_CMDS="
   sed -i -e '/\/usr\/bin\/node/d' ./PKGBUILD
   sed -i -e '/\/usr\/bin\/rg/d' ./PKGBUILD
-'
+"
 make-aur-package cursor-cli
 
 # If the application needs to be manually built that has to be done down here
